f80f85d40c63f2d084ce50f1e11d55e342a9a30a
Project Beam <anthonystoro@icloud.com>
2026-08-16 02:25:00 +0000

    Remove Turnstile from contact form [skip ci]

--- functions/api/config.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  return Response.json({ turnstileSiteKey: context.env.TURNSTILE_SITE_KEY || '' }, {
-    headers: { 'Cache-Control': 'no-store' }
-  });
-}

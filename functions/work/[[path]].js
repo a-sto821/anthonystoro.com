7dc061ee991590deb49d31fbb5a41b681d31a61c
Project Beam <anthonystoro@icloud.com>
2026-08-19 17:42:43 -0400

    Route work URLs to case study entry point

---
+++ functions/work/[[path]].js
@@ -0,0 +1,30 @@
+const VALID_PROJECTS = new Set([
+  'logo-brand-identity',
+  'carlingtech',
+  'fuse-holder-selector',
+  'v-series-selector',
+  'transportation-products-catalog',
+  'conexpo-trade-show',
+  'sp-campbell',
+  'carling-china-capabilities',
+  'fuse-holder-product-overview',
+  'window-switch-product-overview',
+  'cole-hersee-100-year',
+  'carling-100-year-history-booklet',
+  'arbor-meadows',
+  'little-stuess-book'
+]);
+
+export async function onRequest({ request }) {
+  const url = new URL(request.url);
+  const rest = url.pathname.replace(/^\/work\/?/, '');
+  const slug = rest.split('/').filter(Boolean)[0] || '';
+
+  if (!VALID_PROJECTS.has(slug)) {
+    return Response.redirect(new URL('/#work', url.origin), 302);
+  }
+
+  const destination = new URL('/case-study.html', url.origin);
+  destination.searchParams.set('project', slug);
+  return Response.redirect(destination, 302);
+}
